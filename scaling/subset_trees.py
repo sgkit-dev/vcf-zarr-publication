0eb07ca5f9428946c61981c6397b06fd20274282
Jerome Kelleher <jk@well.ox.ac.uk>
2024-03-25 17:58:52 +0000

    Fix some bugs

--- scaling/subset_trees.py
+++ scaling/subset_trees.py
@@ -1,10 +1,10 @@
 import tskit
 
-ts = tskit.load("simulated_chrom_21.ts")
+ts_full = tskit.load("simulated_chrom_21.ts")
 for k in range(1, 7):
     num_individuals = 10**k
     # Assuming diploids
-    ts = ts.simplify(ts.samples()[: 2 * num_individuals])
+    ts = ts_full.simplify(ts_full.samples()[: 2 * num_individuals])
     outfile = f"data/chr21_10_{k}.ts"
     print("Writing", num_individuals, outfile)
     ts.dump(outfile)
