68885dd3e14455583e443d41b45dd33130ac3cfa
willtyler <will.tyler11@gmail.com>
2024-08-25 02:38:41 +0000

    Make program faster

--- software/ts-afdist/ts_afdist.cpp
+++ software/ts-afdist/ts_afdist.cpp
@@ -12,7 +12,7 @@
 using ::tensorstore::Index;
 using ::tensorstore::TensorStore;
 
-absl::Status Run(tensorstore::Spec input_spec) {
+absl::Status Run(tensorstore::Spec &input_spec) {
     TensorStore<int8_t, tensorstore::dynamic_rank, tensorstore::ReadWriteMode::dynamic> store;
     TENSORSTORE_ASSIGN_OR_RETURN(
         store,
@@ -24,61 +24,62 @@ absl::Status Run(tensorstore::Spec input_spec) {
     const Index sample_count = shape[1];
 
     tensorstore::ChunkLayout chunk_layout;
-    TENSORSTORE_ASSIGN_OR_RETURN(
-        chunk_layout, store.chunk_layout());
+    TENSORSTORE_ASSIGN_OR_RETURN(chunk_layout, store.chunk_layout());
 
     tensorstore::ChunkLayout::ReadChunkShape chunk_shape = chunk_layout.read_chunk_shape();
-    const Index variant_chunk_size = chunk_shape[0];
-    const Index sample_chunk_size = chunk_shape[1];
+    const Index variant_chunk_size = std::min(chunk_shape[0], variant_count);
+    const Index sample_chunk_size = std::min(chunk_shape[1], sample_count);
     std::vector<uint64_t> bin_counts(11);
+    std::vector<int8_t> data_vector(variant_chunk_size * sample_chunk_size * 2);
 
-    for (Index variant_chunk_start = 0; variant_chunk_start < variant_count; variant_chunk_start += variant_chunk_size) {
+    for (Index variant_chunk_start = 0; variant_chunk_start < variant_count;
+         variant_chunk_start += variant_chunk_size) {
+        const Index variant_chunk_end = std::min(variant_count, variant_chunk_start + variant_chunk_size);
+        std::vector<uint64_t> ref_counts(variant_chunk_end - variant_chunk_start);
+        std::vector<uint64_t> het_counts(variant_chunk_end - variant_chunk_start);
+        std::vector<uint64_t> hom_alt_counts(variant_chunk_end - variant_chunk_start);
+
         for (Index sample_chunk_start = 0; sample_chunk_start < sample_count; sample_chunk_start += sample_chunk_size) {
-            const Index variant_chunk_end = std::min(variant_count, variant_chunk_start + variant_chunk_size);
             const Index sample_chunk_end = std::min(sample_count, sample_chunk_start + sample_chunk_size);
-            tensorstore::SharedArray<int8_t, tensorstore::dynamic_rank, tensorstore::offset_origin> array;
 
-            TENSORSTORE_ASSIGN_OR_RETURN(array, tensorstore::Read(store | tensorstore::Dims(0, 1).HalfOpenInterval(
-                                                                              {variant_chunk_start, sample_chunk_start},
-                                                                              {variant_chunk_end, sample_chunk_end}))
-                                                    .result());
+            tensorstore::SharedArray<int8_t, 3L, tensorstore::ArrayOriginKind::zero> array =
+                tensorstore::UnownedToShared(tensorstore::Array(
+                    data_vector.data(), {variant_chunk_size, sample_chunk_size, 2}, tensorstore::c_order));
+            tensorstore::Read(
+                store | tensorstore::Dims(0, 1).HalfOpenInterval({variant_chunk_start, sample_chunk_start},
+                                                                 {variant_chunk_end, sample_chunk_end}),
+                array)
+                .Wait();
 
-            for (Index variant_index = variant_chunk_start; variant_index < variant_chunk_end; variant_index++) {
-                uint64_t hom_ref_count = 0;
-                uint64_t hom_alt_count = 0;
-                uint64_t het_count = 0;
-                uint64_t ref_count = 0;
+            for (Index variant_index = 0; variant_index < variant_chunk_end - variant_chunk_start; variant_index++) {
+                for (Index sample_index = 0; sample_index < sample_chunk_end - sample_chunk_start; sample_index++) {
+                    const Index call_index = 2 * (sample_chunk_size * variant_index + sample_index);
+                    const int8_t a = data_vector[call_index];
+                    const int8_t b = data_vector[call_index + 1];
 
-                for (Index sample_index = sample_chunk_start; sample_index < sample_chunk_end; sample_index++) {
-                    const int8_t a = array(variant_index, sample_index, 0);
-                    const int8_t b = array(variant_index, sample_index, 1);
-
-                    if (std::min(a, b) < 0) continue;
-
-                    if (a == b)
-                        if (a == 0)
-                            hom_ref_count += 1;
-                        else
-                            hom_alt_count += 1;
-                    else
-                        het_count += 1;
-
-                    if (a == 0) ref_count += 1;
-                    if (b == 0) ref_count += 1;
+                    ref_counts[variant_index] += (a == 0) + (b == 0);
+                    het_counts[variant_index] += a != b;
+                    hom_alt_counts[variant_index] += a == b && a > 0;
                 }
-
-                const uint64_t alt_count = 2 * sample_count - ref_count;
-                const double alt_freq = alt_count / (2.0 * sample_count);
-                const double het_ref_freq = 2 * alt_freq * (1 - alt_freq);
-                const double hom_alt_freq = alt_freq * alt_freq;
-                Index bin_index = 10 * het_ref_freq;
-
-                bin_counts[bin_index] += het_count;
-
-                bin_index = 10 * hom_alt_freq;
-
-                bin_counts[bin_index] += hom_alt_count;
             }
+        }
+
+        for (Index variant_index = 0; variant_index < variant_chunk_end - variant_chunk_start; variant_index++) {
+            const uint64_t ref_count = ref_counts[variant_index];
+            const uint64_t het_count = het_counts[variant_index];
+            const uint64_t hom_alt_count = hom_alt_counts[variant_index];
+
+            const uint64_t alt_count = 2 * sample_count - ref_count;
+            const double alt_freq = alt_count / (2.0 * sample_count);
+            const double het_ref_freq = 2 * alt_freq * (1 - alt_freq);
+            const double hom_alt_freq = alt_freq * alt_freq;
+            Index bin_index = 10 * het_ref_freq;
+
+            bin_counts[bin_index] += het_count;
+
+            bin_index = 10 * hom_alt_freq;
+
+            bin_counts[bin_index] += hom_alt_count;
         }
     }
 
